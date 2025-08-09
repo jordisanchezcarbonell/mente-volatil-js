b81b647f085bd6d0cd3710f1f818d56a6591aad7
Jordi Sánchez <jsanchez@eunoia.es>
2025-08-09 20:06:05 +0200

    feat: delete

--- lib/format-date.ts
+++
@@ -1,8 +0,0 @@
-export function formatDate(input: string | number | Date, locale = "es-ES") {
-  const d = new Date(input)
-  return new Intl.DateTimeFormat(locale, {
-    year: "numeric",
-    month: "short",
-    day: "2-digit",
-  }).format(d)
-}
--- schemas/index.ts
+++
@@ -1,7 +0,0 @@
-import post from './post';
-import category from './category';
-import author from './author';
-import recap from './recap';
-import settings from './settings';
-
-export const schemaTypes = [post, category, author, recap, settings];
